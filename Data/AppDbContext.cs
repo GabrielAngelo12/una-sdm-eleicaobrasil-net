35177591424a89707823acfaef1565dde695d7a2
GabrielAngelo12 <limagabariel0112@gmail.com>
2026-03-30 20:39:43 -0300

    una-sdm-eleicaobrasilapi

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,14 +11,10 @@ namespace EleicaoBrasilApi.Data
     public class AppDbContext : DbContext
     {
         
-        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
-        {
-            
-
-        }
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options){}
 
         public DbSet<Candidato> Candidatos {get; set;}
-        
+
 
     }
 }
