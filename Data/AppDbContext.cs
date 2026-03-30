78b5e623cdd170b640fa37f000879f0fcfd8b4ef
GabrielAngelo12 <aluno@UNA02DT433798.animaedu.intranet>
2026-03-30 20:30:51 -0300

    una-sdm-eleicaobrasilapi

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using EleicaoBrasilApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EleicaoBrasilApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+            
+
+        }
+
+        public DbSet<Candidato> Candidatos {get; set;}
+        
+
+    }
+}
